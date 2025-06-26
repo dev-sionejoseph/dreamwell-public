69a2ec8be7986f801ed284ad66f62ff7d673fefa
Sione <dev.sionejoseph@gmail.com>
2025-06-26 17:29:58 -0500

    edited cors

--- backend/src/main/java/com/devsione/dreamwell/controller/DreamController.java
+++ backend/src/main/java/com/devsione/dreamwell/controller/DreamController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.UUID;
 
-@CrossOrigin(origins = "http://localhost:5173")
+@CrossOrigin(origins = "https://dreamwell-journal.netlify.app")
 @RestController
 @RequestMapping("/api/dreams")
 public class DreamController {
